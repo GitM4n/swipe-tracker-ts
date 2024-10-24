814c871ac0f4174033313b8ca723f3e77adcee21
GitM4n <korolyov.ahmed@bk.ru>
2024-10-24 12:37:20 +0300

    update

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,7 @@
  * import { createSwipeDetector } from "swipe-detect-js";
  *
  * const swipeobject = document.querySelector('.swipe-object');
- * const detector = createSwipeDetector(swipeobject, options: { sensitivity: 100 }); // Sensitivity is optional
+ * const detector = createSwipeDetector(swipeobject, { sensitivity: 100 }); // Sensitivity is optional
  *
  * detector.swipeLeft(() => {
  *   console.log('Swiped left');
